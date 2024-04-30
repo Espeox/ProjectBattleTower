39dbf66d5731164bb9398c0ccb41bf2dcd2b7c73
Espeox <66660678+Espeox@users.noreply.github.com>
2024-04-30 01:49:37 -0500

    Project Barry Updates
    Updated Rewards, redid select_action(), changed parameters so it actually learn to pick more than one move.

--- TrainerBarry.py
+++ TrainerBarry.py
@@ -1,24 +1,132 @@
 import numpy as np
 from gym.spaces import Space, Box
-from poke_env.player import Gen8EnvSinglePlayer, Gen9EnvSinglePlayer
-from gym.utils.env_checker import check_env
+from poke_env.player import Gen9EnvSinglePlayer
 from poke_env.environment.abstract_battle import AbstractBattle
 from poke_env.environment.side_condition import STACKABLE_CONDITIONS, SideCondition
-from poke_env.player import RandomPlayer, wrap_for_old_gym_api
-from poke_env.player import RandomPlayer
-from poke_env.player import background_evaluate_player
+from poke_env.environment.battle import Battle
+from poke_env.player.battle_order import BattleOrder, ForfeitBattleOrder
 
+#Creates the Gen9 Battle Bot, Barry
 class Barry(Gen9EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    _ACTION_SPACE = list(range(2 * 4 + 6))
+    _DEFAULT_BATTLE_FORMAT = "gen9ou"
+
+    def action_to_move(self, action: int, battle: Battle) -> BattleOrder:  # pyre-ignore
+        """Converts actions to move orders.
+        The conversion is done as follows
+
+        action = -1:
+            The battle will be forfeited.
+        0 <= action < 4:
+            The actionth available move in battle.available_moves is executed.
+        4 <= action < 8:
+            The action - 16th available move in battle.available_moves is executed,
+            while terastallizing.
+        8 <= action < 14
+            The action - 20th available switch in battle.available_switches is executed.
+
+        If the proposed action is illegal, a random legal move is performed.
+        """
+        if action == -1:
+            return ForfeitBattleOrder()
+        elif (
+            action < 4
+            and action < len(battle.available_moves)
+            and not battle.force_switch
+        ):
+            return self.agent.create_order(battle.available_moves[action])
+        elif (
+            battle.can_terastallize
+            and 0 <= action - 4 < len(battle.available_moves)
+            and not battle.force_switch
+        ):
+            return self.agent.create_order(
+                battle.available_moves[action - 4], terastallize=True
+            )
+        elif 0 <= action - 8 < 6:
+            switch_mon = battle.team[list(battle.team.keys())[action-8]]
+            if switch_mon.fainted and not switch_mon.active:
+                return self.agent.choose_random_move(battle)
+            else:
+                return self.agent.create_order(switch_mon)
+        else:
+            return self.agent.choose_random_move(battle)
+
+
+    def capture_replay(self, capture: bool = True):
+        self.agent._save_replays = capture
+        
+    def calc_reward(self, last_battle, current_battle) -> float: #RECREATE
         global battle_tester
         battle_tester = current_battle
-        if current_battle.won:
-            print("Game Won")
-        if current_battle.lost:
-            print("Game Lost")
         return self.reward_computing_helper(
-            current_battle, fainted_value=2.0, hp_value=1.0, victory_value=30.0
+            current_battle, fainted_value=4.0, hp_value=1.0, status_value=3.0, hazard_value=2.0, victory_value=20.0
         )
+    def reward_computing_helper(
+        self,
+        battle: AbstractBattle,
+        *,
+        fainted_value: float = 0.0,
+        hp_value: float = 0.0,
+        number_of_pokemons: int = 6,
+        starting_value: float = 0.0,
+        status_value: float = 0.0,
+        victory_value: float = 1.0,
+        hazard_value: float = 1.0
+    ) -> float:
+        """A helper function to compute rewards.
+
+        :param battle: The battle for which to compute rewards.
+        :type battle: AbstractBattle
+        :param fainted_value: The reward weight for fainted pokemons. Defaults to 0.
+        :type fainted_value: float
+        :param hp_value: The reward weight for hp per pokemon. Defaults to 0.
+        :type hp_value: float
+        :param number_of_pokemons: The number of pokemons per team. Defaults to 6.
+        :type number_of_pokemons: int
+        :param starting_value: The default reference value evaluation. Defaults to 0.
+        :type starting_value: float
+        :param status_value: The reward value per non-fainted status. Defaults to 0.
+        :type status_value: float
+        :param victory_value: The reward value for winning. Defaults to 1.
+        :type victory_value: float
+        :return: The reward.
+        :rtype: float
+        """
+        if battle not in self._reward_buffer:
+            self._reward_buffer[battle] = starting_value
+        current_value = 0
+
+        for mon in battle.team.values():
+            current_value += mon.current_hp_fraction * hp_value
+            if mon.fainted:
+                current_value -= fainted_value
+            elif mon.status is not None:
+                current_value -= status_value
+
+        current_value += (number_of_pokemons - len(battle.team)) * hp_value
+
+        for mon in battle.opponent_team.values():
+            current_value -= mon.current_hp_fraction * hp_value
+            if mon.fainted:
+                current_value += fainted_value
+            elif mon.status is not None:
+                current_value += status_value
+
+        current_value -= (number_of_pokemons - len(battle.opponent_team)) * hp_value
+
+        current_value += sum(list(battle.side_conditions.values())) * hazard_value
+        current_value -= sum(list(battle.opponent_side_conditions.values())) * hazard_value
+
+        if battle.won:
+            current_value += victory_value
+        elif battle.lost:
+            current_value -= victory_value
+
+        to_return = current_value - self._reward_buffer[battle]
+        self._reward_buffer[battle] = current_value
+
+        return to_return
     
     def encode_status(self, status):
         if status == None:
@@ -46,7 +154,7 @@ class Barry(Gen9EnvSinglePlayer):
         battle = []
         #Active Field
         battle += [0]*14
-        battle += [0, 0, 0, 0, 0, 100, 1, 0]*6
+        battle += [0, 0, 0, 0, 0, 1, 1, 0]*6
         battle += battle
         return np.float32(battle) #size 124
 
@@ -85,7 +193,7 @@ class Barry(Gen9EnvSinglePlayer):
         for p in list(battle.team):
             poke = battle.team[p]
             poke_info = self.encode_status(poke.status)# Len 5
-            poke_info.append(poke.current_hp_fraction*100) # Len 1
+            poke_info.append(poke.current_hp_fraction) # Len 1
             poke_info.append(int(poke.item != "")) # Len 1
             poke_info.append(int(poke.terastallized)) # Len 1
             pokemon_team = pokemon_team + poke_info # Len 48
@@ -114,7 +222,7 @@ class Barry(Gen9EnvSinglePlayer):
             if ("p2: " + p.species.capitalize()) in battle.opponent_team:
                 opponent_pokemon = battle.opponent_team["p2: " + p.species.capitalize()]
                 op_poke_info = self.encode_status(opponent_pokemon.status)
-                op_poke_info.append(opponent_pokemon.current_hp_fraction*100) #Check if needs to be fraction?
+                op_poke_info.append(opponent_pokemon.current_hp_fraction)
                 op_poke_info.append(int(opponent_pokemon.item != ""))
                 op_poke_info.append(int(opponent_pokemon.terastallized))
             #opponent_pokemon = battle.opponent_team["p2: " + p.species.capitalize()]
@@ -156,7 +264,7 @@ class Barry(Gen9EnvSinglePlayer):
         poke_high += [1, 1, 1, 1, 1]
         #HP
         poke_low += [0]
-        poke_high += [100] 
+        poke_high += [1] 
         #Has_Item
         poke_low += [0]
         poke_high += [1]
